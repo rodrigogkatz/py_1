8891f578006db636f4c3ece0da0ca78b4ebfad4e
rodrigogkatz <rodrigogkatz@gmail.com>
2022-05-29 16:52:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #Imports externos
 from datetime import date
+from datetime import datetime
 
 #Imports internos
 import constantes
@@ -30,6 +31,36 @@ def pedirDato(tipoDeDato, primeraPregunta, minDato, maxDato, mensajeDeError, pre
 		return dato
 	#float, double statements
 
+def definirMes(numeroDeMes, minMes, maxMes, mensajeDeError):
+	mes = ""
+	if numeroDeMes < constantes.UNO or numeroDeMes > constantes.MAXMES:
+		print(mensajeDeError)
+	elif numeroDeMes == 1:
+		mes = "enero"
+	elif numeroDeMes == 2:
+		mes = "febrero"
+	elif numeroDeMes == 3:
+		mes = "marzo"
+	elif numeroDeMes == 4:
+		mes = "abril"
+	elif numeroDeMes == 5:
+		mes = "mayo"
+	elif numeroDeMes == 6:
+		mes = "junio"
+	elif numeroDeMes == 7:
+		mes = "julio"
+	elif numeroDeMes == 8:
+		mes = "agosto"
+	elif numeroDeMes == 9:
+		mes = "septiembre"
+	elif numeroDeMes == 10:
+		mes = "octubre"
+	elif numeroDeMes == 11:
+		mes = "noviembre"
+	elif numeroDeMes == 12:
+		mes = "diciembre"
+	return mes
+
 nombre = pedirDato("string", "Ingrese su nombre: ", constantes.MINNOMBRE, constantes.MAXNOMBRE, "ERROR: El nombre debe tener caracteres alfabeticos y debe tener entre 2 y 150 caracteres", "Ingrese nuevamente su nombre: ", constantes.REINTENTOS)
 edad = pedirDato("entero", "Ingrese su edad: ", constantes.MINEDAD, constantes.MAXEDAD, "ERROR: La edad debe ser un numero entero. Y debe ser mayor de 18 y menor de 120", "Ingrese nuevamente su edad: ", constantes.REINTENTOS)
 diaDeNacimiento = pedirDato("entero", "Dia de nacimiento: ", constantes.UNO, constantes.MAXDIA, "ERROR: El dia debe ser un numero entero entre 1 y 31", "Ingrese nuevamente el dia de nacimiento: ", constantes.REINTENTOS)
@@ -39,8 +70,18 @@ anoActual = date.today().year
 mesActual = date.today().month
 diaActual = date.today().day
 
+#horarioActual = now.strftime("%H:%M:%S")
+horaActual = datetime.now().hour
+minutoActual = datetime.now().minute
+segundoActual = datetime.now().second
+
+#print("Current Time is :", horarioActual)
+print("Current hor is :", horaActual)
+print("Current min is :", minutoActual)
+print("Current seg is :", segundoActual)
+
 print("---------------------------------------------")
-print("    Hoy es el dia " + str(diaActual) + ", del mes " + str(mesActual) + ", año " + str(anoActual))
+print("    Hoy es el dia " + str(diaActual) + " de " + definirMes(mesActual, constantes.UNO, constantes.MAXMES, "ERROR: El mes debe ser un numero entero entre 1 y 12") + " del año " + str(anoActual) + ".\n\tHora " + str(horaActual) + ":" + str(minutoActual) + ":" + str(segundoActual))
 print("---------------------------------------------")
 print("\tBuen dia " + nombre)
 
