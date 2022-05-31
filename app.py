f25fe320ab64aee5a4e4152e2c2d2ee12d33a0b3
rodrigogkatz <rodrigogkatz@gmail.com>
2022-05-31 11:24:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,18 +44,42 @@ def calcularAnoDeNacimiento(diaDeNacimiento, mesDeNacimiento, edad, diaActual, m
 		anoDeNacimiento = anoActual - edad - constantes.UNO
 	return anoDeNacimiento
 
-def pedirDato(tipoDeDato, primeraPregunta, minDato, maxDato, mensajeDeError, preguntaDespuesDeError, reintentos):
+def pedirCodigoDeDesbloqueo(mensajeDeDesbloqueo, errorCodigoDeDesbloque, mensajeDesbloqueoSatisfactorio):
+	resultado = 1
+	codigoDeDesbloqueo = input(mensajeDeDesbloqueo)
+	while (len(codigoDeDesbloqueo) > 14 or len(codigoDeDesbloqueo) < 14) and codigoDeDesbloqueo != "Desbloqueo1234":
+		resultado = -1
+		codigoDeDesbloqueo = input(errorCodigoDeDesbloqueo)
+
+	if resultado == -1:
+		print(errorCodigoDeDesbloqueo)
+	elif resultado == 1:
+		print(mensajeDesbloqueoSatisfactorio)
+
+	return resultado
+
+def pedirDato(tipoDeDato, primeraPregunta, minDato, maxDato, mensajeDeError, preguntaDespuesDeError, reintentos, reintentosError, codigoDeDesbloqueo):
 	if tipoDeDato == 'string':
 		dato = input(primeraPregunta)
 		while len(dato) <= minDato or dato.isalpha() == False:
-			print(mensajeDeError)
-			dato = input(preguntaDespuesDeError)
+			if reintentos > 0:
+				print(mensajeDeError)
+				reintentos = reintentos - constantes.UNO
+				print(tipoDeDato + ": " + str(reintentos) + " " + reintentosError)
+				dato = input(preguntaDespuesDeError)
+			if reintentos < 0:
+				reintentos = 3
+				resultado = pedirCodigoDeDesbloqueo("Ingrese el codigo de desbloqueo para continuar (Desbloqueo1234): ", "El codigo de debloqueo no funciona, contacte al administrador del sitema... JODETE HERMANO", "El flujo fue desbloqueado")
+				
 		return dato
 	if tipoDeDato == 'entero':
 		dato = int(input(primeraPregunta))
 		while type(dato) != int or dato < minDato or dato > maxDato:
-			print(mensajeDeError)
-			dato = int(input(preguntaDespuesDeError))
+			if reintentos > 0:
+				print(mensajeDeError)
+				reintentos = reintentos - constantes.UNO
+				print(tipoDeDato + ": " + str(reintentos) + " " + reintentosError)
+				dato = int(input(preguntaDespuesDeError))
 		return dato
 	#float, double statements
 
@@ -97,13 +121,6 @@ input() siempre devuelve un string
 Si quiero hacer un comment de una sola linea tengo que usar # como en los casos de abajo
 """
 
-mensajear("Ingrese los siguientes datos:")
-nombre = pedirDato("string", "Nombre: ", constantes.MINNOMBRE, constantes.MAXNOMBRE, "ERROR: El nombre debe tener caracteres alfabeticos y debe tener entre 2 y 150 caracteres", "Ingrese nuevamente su nombre: ", constantes.REINTENTOS)
-apellido = pedirDato("string", "Apellido: ", constantes.MINNOMBRE, constantes.MAXNOMBRE, "ERROR: El apellido debe tener caracteres alfabeticos y debe tener entre 2 y 150 caracteres", "Ingrese nuevamente su apellido: ", constantes.REINTENTOS)
-edad = pedirDato("entero", "Edad: ", constantes.MINEDAD, constantes.MAXEDAD, "ERROR: La edad debe ser un numero entero. Y debe ser mayor de 18 y menor de 120", "Ingrese nuevamente su edad: ", constantes.REINTENTOS)
-diaDeNacimiento = pedirDato("entero", "Dia de nacimiento: ", constantes.UNO, constantes.MAXDIA, "ERROR: El dia debe ser un numero entero entre 1 y 31", "Ingrese nuevamente el dia de nacimiento: ", constantes.REINTENTOS)
-mesDeNacimiento = pedirDato("entero", "Mes de nacimiento: ", constantes.UNO, constantes.MAXMES, "ERROR: El mes debe ser un numero entero entre 1 y 12", "Ingrese nuevamente el mes de nacimiento: ", constantes.REINTENTOS)
-
 anoActual = date.today().year
 mesActual = date.today().month
 diaActual = date.today().day
@@ -113,6 +130,14 @@ horaActual = datetime.now().hour
 minutoActual = datetime.now().minute
 segundoActual = datetime.now().second
 
+mensajear("Ingrese los siguientes datos:")
+nombre = pedirDato("string", "Nombre: ", constantes.MINNOMBRE, constantes.MAXNOMBRE, "ERROR: El nombre debe tener caracteres alfabeticos y debe tener entre 2 y 150 caracteres", "Ingrese nuevamente su nombre: ", constantes.REINTENTOS, "reintentos disponibles", "Desbloqueo1234")
+apellido = pedirDato("string", "Apellido: ", constantes.MINNOMBRE, constantes.MAXNOMBRE, "ERROR: El apellido debe tener caracteres alfabeticos y debe tener entre 2 y 150 caracteres", "Ingrese nuevamente su apellido: ", constantes.REINTENTOS, "reintentos disponibles", "Desbloqueo1234")
+edad = pedirDato("entero", "Edad: ", constantes.MINEDAD, constantes.MAXEDAD, "ERROR: La edad debe ser un numero entero. Y debe ser mayor de 18 y menor de 120", "Ingrese nuevamente su edad: ", constantes.REINTENTOS, "reintentos disponibles", "Desbloqueo1234")
+diaDeNacimiento = pedirDato("entero", "Dia de nacimiento: ", constantes.UNO, constantes.MAXDIA, "ERROR: El dia debe ser un numero entero entre 1 y 31", "Ingrese nuevamente el dia de nacimiento: ", constantes.REINTENTOS, "reintentos disponibles", "Desbloqueo1234")
+mesDeNacimiento = pedirDato("entero", "Mes de nacimiento: ", constantes.UNO, constantes.MAXMES, "ERROR: El mes debe ser un numero entero entre 1 y 12", "Ingrese nuevamente el mes de nacimiento: ", constantes.REINTENTOS, "reintentos disponibles", "Desbloqueo1234")
+
 mensajear("Resultados:")
 imprimirResultados(diaActual, mesActual, constantes.UNO, constantes.MAXMES, "ERROR: El mes debe ser un numero entero entre 1 y 12", anoActual, horaActual, minutoActual, segundoActual, nombre, apellido, diaDeNacimiento, mesDeNacimiento, edad)
+
 
